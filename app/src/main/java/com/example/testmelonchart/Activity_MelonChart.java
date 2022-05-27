049993082ecbd3c6aaba5cb43e009410726908b2
rkdgus19 <rkdgus0119@gmail.com>
2022-05-27 15:01:11 +0900

    220527.01

---
+++ app/src/main/java/com/example/testmelonchart/Activity_MelonChart.java
@@ -0,0 +1,99 @@
+package com.example.testmelonchart;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.annotation.Nullable;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Activity_MelonChart extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerAdapter adapter;
+    String melon_chart_url = "https://www.melon.com/chart/";
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_melon_chart);
+
+        recyclerView = findViewById(R.id.recyclerView_chart);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+
+        getData();
+    }
+
+    private void getData(){
+        MelonJsoup jsoupAsyncTask = new MelonJsoup();
+        jsoupAsyncTask.execute();
+    }
+
+    private class MelonJsoup extends AsyncTask<Void, Void, Void> {
+        ArrayList<String> listTitle = new ArrayList<>();
+        ArrayList<String> listName = new ArrayList<>();
+        ArrayList<String> listUrl = new ArrayList<>();
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Document doc = Jsoup.connect(melon_chart_url).get();
+                final Elements rank_list1 = doc.select("div.wrap_song_info div.ellipsis.rank01 span a");
+                final Elements rank_list_name = doc.select("div.wrap_song_info div.ellipsis.rank02 span a");
+
+                final Elements image_list1 = doc.select("tr#lst50.lst50 div.wrap a.image_typeAll img");
+                Handler handler = new Handler(Looper.getMainLooper()); // 객체생성
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //순위정보
+                        for(Element element: rank_list1) {
+                            listTitle.add(element.text());
+                        }
+                        //가수정보
+                        for (Element element : rank_list_name) {
+                            listName.add(element.text());
+                        }
+                        // 이미지정보
+                        for (Element element : image_list1){
+                            listUrl.add(element.attr("src"));
+                        }
+
+                        for (int i = 0; i < 30 ; i++) {
+                            ChartDTO data = new ChartDTO();
+                            data.setTitle(listTitle.get(i));
+                            data.setImageUrl(listUrl.get(i));
+                            data.setRankNum(String.valueOf(i+1));
+                            data.setName(listName.get(i));
+
+                            adapter.addItem(data);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/testmelonchart/RecyclerAdapter.java
@@ -0,0 +1,65 @@
+package com.example.testmelonchart;
+
+import androidx.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
+
+    private ArrayList<ChartDTO> listData = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_chart, viewGroup, false);
+        return new
+                ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.ItemViewHolder itemViewHolder, int i) {
+        itemViewHolder.onBind(listData.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+    void addItem(ChartDTO data) {
+        // 외부에서 item을 추가시킬 함수입니다.
+        listData.add(data);
+    }
+    class ItemViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView txt_ranktNum, txt_chartName, txt_chartTitle;
+        private ImageView img_chart;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txt_chartTitle = itemView.findViewById(R.id.txt_chartTitle);
+            txt_chartName = itemView.findViewById(R.id.txt_chartName);
+            txt_ranktNum  = itemView.findViewById(R.id.txt_ranktNum);
+            img_chart     = itemView.findViewById(R.id.img_chart);
+
+        }
+
+        void onBind(ChartDTO data){
+            txt_ranktNum.setText(data.getRankNum());
+            txt_chartName.setText(data.getName());
+            txt_chartTitle.setText(data.getTitle());
+
+            Glide.with(itemView.getContext()).load(data.getImageUrl()).into(img_chart);
+
+        }
+    }
+}
